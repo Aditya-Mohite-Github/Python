10fe0d13b6187a06f975a052abb89a94ade6e3cf
Aditya Mohite <mohiteaditya689@gmail.com>
2024-04-02 21:16:18 +0530

    Uploaded on 02 April 2024

---
+++ CLU/main.py
@@ -0,0 +1,31 @@
+import argparse
+import weatherandtimeutils
+import locationutils
+
+parser = argparse.ArgumentParser()
+
+# 'weather' as a positional argument (mandatory)
+parser.add_argument('weather', help="Main command keyword")
+
+# argument for version
+parser.add_argument('-v', '--version', action='store_true' , help="Outputs the Version of the Application")
+
+# argument for location for it's weather and current for exact location weather with help of co-ordinates using geocoder in locationutils.py
+parser.add_argument('mode',nargs='?'  ,choices=['location', 'current'], help="Specify mode: 'location' to get weather by coordinates or 'current' to get weather for current location")
+parser.add_argument('locationarg' , nargs='?', help="Argument if location is Entered")
+
+
+args = parser.parse_args()
+
+if args.version: print("Weather Utility ver 1.1")
+
+if args.mode == 'location':
+    if args.locationarg:
+        weatherandtimeutils.get_forecast_by_location(args.locationarg)
+    else: print("Location Not Provided")
+elif args.mode == 'current':
+    coordinates = locationutils.get_current_location()
+    print(coordinates[0] , coordinates[1])
+    weatherandtimeutils.get_forecast_by_coordinates(coordinates[0] , coordinates[1])
+
+
